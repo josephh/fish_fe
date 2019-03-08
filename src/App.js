cd9a35b8c68487f7a9302ca25694f465a4ec561c
josephh <josephh@rocketmail.com>
2019-03-08 14:12:11 +0000

    state stuff

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@
 it.
 * Both coding styles below are valid:  1. returning inline JSX, 2. calling React.createElement
 */
-import React, {
-  useState
-} from 'react'; // useState is part of 'React hooks'
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import NavigationCard from './NavigationCard/NavigationCard';
 
-const app = props => {
+class App extends Component {
 
   // use state returns an array with EXACTLY 2 elements: 1. the current state, 2. a function to amend state
   // With React hooks, tbere is no AUTOMATIC merge of state (as there is with OOTB react).
-  const [cardsState, setCardsState] = useState({
+  state = {
     cards: [{
         title: "See What's Been Hooked!",
         link: "/hooked"
@@ -31,11 +29,10 @@ const app = props => {
       }
     ],
     showCards: false
-  });
+  };
 
-  const // prefer es6 arrow functions here as it avoids problems with this keyword
-    switchTitleHandler = () => {
-      setCardsState({
+  switchTitleHandler = () => {
+      this.setState({
         cards: [{
             title: "See What's Been cooked!",
             link: "/hooked"
@@ -52,8 +49,8 @@ const app = props => {
       })
     }
 
-  const titleChangedHandler = (event) => {
-    setCardsState({
+  titleChangedHandler = (event) => {
+    this.setState({
       cards: [{
           title: "see what's been cooked",
           link: "/hooked"
@@ -70,57 +67,40 @@ const app = props => {
     })
   }
 
-  const toggleNavigationCardsHandler = () => {
-    const doesShow = cardsState.showCards;
-    setCardsState({showCards: !doesShow });
+  toggleNavigationCardsHandler = () => {
+    const doesShow = this.state.showCards;
+    this.setState({showCards: !doesShow});
   }
 
-  // render() { // this is the most important thing that each React component needs to do...but only required for classes extending 'Component'
-
-  // don't add parentheses after the call to the handler - it's to be registered not immediately invoked!
-  return (
+  render = () =>  { return (
     <div className = "App">
       <h1> Hi </h1>
       <div>
-        <button onClick = { toggleNavigationCardsHandler }> Switch Title </button>
+        <button onClick = { this.toggleNavigationCardsHandler }> Switch Title </button>
       </div>
-      {
-        cardsState.showCards ?
-          <div>
-            <NavigationCard
-              title = { cardsState.cards[0].title }
-              link = { cardsState.cards[0].link }
-              changed = { titleChangedHandler }/>
-            <NavigationCard
-              title = { cardsState.cards[1].title }
-              link = { cardsState.cards[1].link }
-              changed = { titleChangedHandler }>
-              <strong> something in between </strong>
-            </NavigationCard>
-            <NavigationCard
-              title = { cardsState.cards[2].title }
-              link = { cardsState.cards[2].link }
-              changed = { titleChangedHandler }>
-            </NavigationCard>
-          </div>
-        : null
-      }
+      {this.state.showCards ?
+        <div>
+          <NavigationCard
+            title = {this.state.cards[0].title}
+            link = {this.state.cards[0].link}
+            changed = {this.titleChangedHandler}/>
+          <NavigationCard
+            title = {this.state.cards[1].title}
+            link = {this.state.cards[1].link}
+            changed = {this.titleChangedHandler}>
+            <strong> something in between </strong>
+          </NavigationCard>
+          <NavigationCard
+            title = {this.state.cards[2].title}
+            link = {this.state.cards[2].link}
+            changed = {this.titleChangedHandler}>
+          </NavigationCard>
+        </div>
+      : null}
     </div>
-  )
+  )}
   // : JSX provides a nicer coding approach
-
-
-  // the following line means add an element and nest any no. of child elements inside it...
-  // React.createElement('div', null, 'h1', 'It\'s a react app!?') // the nested h1 in the code exmaple  is interpreted as text,
-  // instead we need to call createElement again
-  // return React.createElement(
-  //   'div', {className: 'App'} /*options*/, 'Hi!',
-  //     React.createElement('h1', null /*options*/, 'Does this bit appear?'),
-  //     NavigationCard({title: "See What\'s Been Hooked!"}), // since this is a function
-  //     NavigationCard({title: "Upload a Catch"}) // since this is a function
-  //   )
-
 
 }
 
-export default app; // export the class by default
+export default App; // export the class by default
